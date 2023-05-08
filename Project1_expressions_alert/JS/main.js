6418b92952ec407ceaf7674a0f346e9766cff617
Daniel Mieleniewski <daniel.mieleniewski@gmail.com>
2023-05-08 19:00:46 +0100

    first JS project

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,17 @@
+var sent1 = "This is the beggining of the string";
+var sent2 = "and this is the end of the string" //setting variables
+var sent3 = "Output message"
+
+
+
+window.alert("This is a basic JS project") //Pop up window
+
+
+document.write(sent1+sent2); //Concatenating two variables
+
+
+function My_First_Function() { //Button function
+    var str= ""; //Input desired button outpur text in ""
+    document.getElementById("Button_Text").innerHTML=str; 
+    document.write(sent3);
+}
